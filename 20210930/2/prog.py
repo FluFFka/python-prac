9aaff3e129e4d007f3e051b9a3ac953eb6f69260
FluFFka <iliasfedorenko@mail.ru>
2021-09-30 15:39:00 +0300

    Upgrade solve for task 2
    - Made solve for 2 task online

--- 20210930/2/prog.py
+++ 20210930/2/prog.py
@@ -1,2 +1,1 @@
-a, b = eval(input())
-print([j for j in range(a, b) if j > 1 and all([j%i for i in range(2, int(j**0.5)+1)])])
+print([j for j in range(*eval(input())) if j > 1 and all([j%i for i in range(2, int(j**0.5)+1)])])
