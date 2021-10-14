15fa28e95b52ccadc29fd8a61f902d22cd6a0e6c
FluFFka <iliasfedorenko@mail.ru>
2021-10-14 22:31:19 +0300

    Solved 1 task
    - Modified solve for 1 task
    - Added tests for 1 task

--- 20211014/1/prog.py
+++ 20211014/1/prog.py
@@ -1,19 +1,21 @@
 from fractions import Fraction
-inp = input().strip().split(',')
-s, w, degA = Fraction(inp[0]), Fraction(inp[1]), int(inp[2])
-A = []
-for i in range(3, 4 + degA):
-    A.append(Fraction(inp[i]))
-degB = int(inp[4 + degA])
-B = []
-for i in range(5 + degA, len(inp)):
-    B.append(Fraction(inp[i]))
-resA = 0
-resB = 0
-for i in range(degA + 1):
-    resA += s**i * A[degA - i - 1]
-for i in range(degB + 1):
-    resB += s**i * B[degB - i - 1]
-if resB == 0:
-    print(False)
-print(resA / resB == w)
+def Fun(*inp):
+    s, w, degA = Fraction(inp[0]), Fraction(inp[1]), int(inp[2])
+    A = []
+    for i in range(3, 4 + degA):
+        A.append(Fraction(inp[i]))
+    degB = int(inp[4 + degA])
+    B = []
+    for i in range(5 + degA, len(inp)):
+        B.append(Fraction(inp[i]))
+    resA = 0
+    resB = 0
+    for i in range(degA + 1):
+        resA += s**i * A[degA - i]
+    for i in range(degB + 1):
+        resB += s**i * B[degB - i]
+    if resB == 0:
+        return False
+    return resA / resB == w
+a = input().strip().split(',')
+print(Fun(*a))
