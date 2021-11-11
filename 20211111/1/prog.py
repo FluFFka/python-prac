3f4132fa2b388767745f323a46241deb6f293206
FluFFka <iliasfedorenko@mail.ru>
2021-11-11 16:07:20 +0300

    Practice results

---
+++ 20211111/1/prog.py
@@ -0,0 +1,11 @@
+class OrderedComplex(complex):
+    def __lt__(self, other):
+        return self.real < other.real and self.imag < other.imag
+
+class OrderedComplexMul(complex):
+    def __matmul__(self, other):
+        return self.real * other.real + self.imag * other.imag
+
+A, B = OrderedComplex(1, -5), OrderedComplexMul(10, 15)
+print(A<B, B@B)
+
---
+++ 20211111/2/prog.py
@@ -0,0 +1,38 @@
+class InvalidInput(Exception):
+    pass
+class BadTriangle(Exception):
+    pass
+
+class TriangleSquare:
+    a, b, c = None, None, None
+    def __init__(self, inpStr):
+        try:
+            (x1, y1), (x2, y2), (x3, y3) = eval(inpStr)
+        except Exception:
+            raise InvalidInput
+        try:
+            self.a = (abs(x1 - x2)**2 + abs(y1 - y2)**2)**0.5
+            self.b = (abs(x3 - x2)**2 + abs(y3 - y2)**2)**0.5
+            self.c = (abs(x1 - x3)**2 + abs(y1 - y3)**2)**0.5
+        except Exception:
+            raise BadTriangle
+        try:
+            s = self.get_square() <= 0
+        except Exception as E:
+            print(E)
+            raise BadTriangle
+        if s:
+            raise BadTriangle
+    def get_square(self):
+        p = 0.5 * (self.a + self.b + self.c)
+        return (p * (p - self.a) * (p - self.b) * (p - self.c))**0.5
+while True:
+    try:
+        tri = TriangleSquare(input())
+    except InvalidInput:
+        print("Invalid input")
+    except BadTriangle:
+        print("Not a triangle")
+    else:
+        print(f"{tri.get_square():.2f}")
+        break
