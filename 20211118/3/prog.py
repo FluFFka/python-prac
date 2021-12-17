da8005958ec894a4a458c081ea3b3d8ba09fb614
FluFFka <iliasfedorenko@mail.ru>
2021-12-17 20:05:44 +0300

    Fixed 20211118/3 and 20211125/2

--- 20211118/3/prog.py
+++ 20211118/3/prog.py
@@ -1,28 +1,31 @@
 from string import ascii_lowercase
 
+al = [*ascii_lowercase]
+
+
 class Alpha:
-    __slots__ = [*ascii_lowercase]
+    __slots__ = al
     def __init__(self, **kwargs):
         for key, value in kwargs.items():
             setattr(self, key, value)
     def __str__(self):
         return ', '.join([f"{key}: {getattr(self, key)}" for key in self.__slots__ if hasattr(self, key)])
 
-class EMPTYCLASS: pass
 
-class AlphaQ(EMPTYCLASS):
+class AlphaQ:
     def __init__(self, **kwargs):
-        if not all(key in ascii_lowercase for key in kwargs):
-            raise AttributeError
+        for key in kwargs:
+            if not (key in al):
+                raise AttributeError(f"'AlphaQ' object has no attribute '{key}'")
         for key, value in kwargs.items():
             setattr(self, key, value)
     def __setattr__(self, key, value):
-        if key in ascii_lowercase:
+        if key in al:
             super().__setattr__(key, value)
         else:
-            raise AttributeError
+            raise AttributeError(f"'AlphaQ' object has no attribute '{key}'")
     def __str__(self):
-        return ', '.join([f"{key}: {getattr(self, key)}" for key in ascii_lowercase if hasattr(self, key)])
+        return ', '.join([f"{key}: {getattr(self, key)}" for key in al if hasattr(self, key)])
 
 import sys
 exec(sys.stdin.read())
--- 20211125/2/prog.py
+++ 20211125/2/prog.py
@@ -1,2 +1,2 @@
 import sys
-print(sys.stdin.buffer.read().decode(errors='replace').encode('latin1', 'replace').decode('cp1251', 'replace'))
+print(sys.stdin.buffer.read().decode(errors='replace').encode('latin1', 'replace').decode('cp1251', 'replace'), end='')
