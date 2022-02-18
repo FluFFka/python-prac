5c6ad0637c7c7b32db367ea2fa6464227b853e0f
FluFFka <iliasfedorenko@mail.ru>
2022-02-18 22:51:46 +0300

    Commit story

--- 20220214/1/prog.py
+++ 20220214/1/prog.py
@@ -27,6 +27,24 @@ def draw_tree(repo_path, tree_id, level=0):
         if tmode == b'40000':
             draw_tree(repo_path, num.hex(), level + 1)
 
+def commit_story(repo_path, commit_id):
+    while 1:
+        _, commit_body = get_object_info(repo_path, commit_id)
+        commit_info = commit_body.decode()
+        print(f"COMMIT: {commit_id}")
+        print(commit_info)
+        
+        commit_info = commit_info.split()
+        tree_ind = commit_info.index('tree')
+        tree_id = commit_info[tree_ind + 1]
+        print(f"\nTREE: {tree_id}")
+        draw_tree(repo_path, tree_id)
+        if 'parent' not in commit_info:
+            break
+        parent_ind = commit_info.index('parent')
+        commit_id = commit_info[parent_ind + 1]
+        print('\n')
+
 if len(sys.argv) <= 1:
     print(', '.join(listdir(branches_path)))
 else:
@@ -34,10 +52,4 @@ else:
     branch_path = join(branches_path, branch_name)
     with open(branch_path, "r") as f:
         branch_id = f.read().strip()
-    _, commit_body = get_object_info(repo_path, branch_id)
-    commit_info = commit_body.decode()
-    print(commit_info)
-    
-    tree_id = commit_info.split()[1]
-    draw_tree(repo_path, tree_id)
-    
+    commit_story(repo_path, branch_id)    
