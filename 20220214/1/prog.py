d4c22ad5163aaa21be3f7273cc66f0df73fbc10d
FluFFka <iliasfedorenko@mail.ru>
2022-02-18 22:27:09 +0300

    Last tree

--- 20220214/1/prog.py
+++ 20220214/1/prog.py
@@ -7,6 +7,14 @@ import zlib
 repo_path = join('..', '..')
 branches_path = join(repo_path, '.git', 'refs', 'heads')
 
+def get_object_info(repo_path, object_id):
+    object_path = join(repo_path, '.git', 'objects', object_id[0:2], object_id[2:])
+    with open(object_path, 'rb') as object_file:
+        dec = zlib.decompress(object_file.read())
+        header, _, body = dec.partition(b'\x00')
+        kind, _ = header.split()
+    return kind, body
+
 if len(sys.argv) <= 1:
     print(', '.join(listdir(branches_path)))
 else:
@@ -14,10 +22,15 @@ else:
     branch_path = join(branches_path, branch_name)
     with open(branch_path, "r") as f:
         branch_id = f.read().strip()
-    last_commit_path = join(repo_path, '.git', 'objects', branch_id[0:2], branch_id[2:])
-    with open(last_commit_path, 'rb') as commit_file:
-        commit_dec = zlib.decompress(commit_file.read())
-        header, _, body = commit_dec.partition(b'\x00')
-        kind, size = header.split()
-        if kind == b'commit':
-            print(body.decode())
+    _, commit_body = get_object_info(repo_path, branch_id)
+    commit_info = commit_body.decode()
+    print(commit_info)
+    tree_id = commit_info.split()[1]
+    _, tree_body = get_object_info(repo_path, tree_id)
+    
+    tail = tree_body
+    while tail:
+        treeobj, _, tail = tail.partition(b'\x00')
+        tmode, tname = treeobj.split()
+        num, tail = tail[:20], tail[20:]
+        print(f"{tname.decode()} {tmode.decode()} {num.hex()}")
