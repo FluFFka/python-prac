08e04ad8995244e74d6ab0d2e24560c3ee4cf0e9
FluFFka <iliasfedorenko@mail.ru>
2022-03-03 16:01:56 +0300

    Added parameter processing

--- 20220228/1/prog.py
+++ 20220228/1/prog.py
@@ -1,7 +1,12 @@
 import textdistance
 
 def dist(s1, s2, t):
-    return textdistance.levenshtein(s1, s2)
+    if t == "L":
+        return textdistance.levenshtein(s1, s2)
+    elif t == "D":
+        return textdistance.damerau_levenshtein(s1, s2)
+    else:
+        return -1
 
 s1 = input()
 s2 = input()
