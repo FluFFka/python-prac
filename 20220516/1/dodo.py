dcfce81eb84e5088df4d8c6b492da91407468a92
FluFFka <iliasfedorenko@mail.ru>
2022-05-26 00:25:29 +0300

    Source code build

--- 20220516/1/dodo.py
+++ 20220516/1/dodo.py
@@ -1,5 +1,5 @@
 DOIT_CONFIG = {
-    'default_tasks': ['extract', 'update', 'compile', 'test']#, 'wheel', 'sdist', 'cleanup']
+    'default_tasks': ['extract', 'update', 'compile', 'test']
 }
 
 def task_extract():
@@ -39,33 +39,4 @@ def task_test():
     return {
         "actions": ["python3 -m tests"]
     }
-'''
-def task_wheel():
-    """
-    Build the wheel.
-    """
-    return {
-        "actions": ["python3 -m build -w"],
-        "file_dep": ["eq", "po/ru/LC_MESSAGES/eq.mo"],
-        "targets": ["dist/eq-0.0.1-py3-none-any.whl"]
-    }
 
-def task_sdist():
-    """
-    Build a sdist.
-    """
-    return {
-        "actions": ["python3 -m build -s"],
-        "file_dep": ["eq", "po/ru/LC_MESSAGES/eq.mo"],
-        "targets": ["dist/eq-0.0.1.tar.gz"]
-    }
-
-def task_cleanup():
-    """
-    Remove extra files.
-    """
-    return {
-        "actions": ["rm po/eq.pot",
-                    "rm po/ru/LC_MESSAGES/eq.mo"]
-    }
-'''
